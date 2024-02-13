57c4c757472e40cac9f822d489f29929f95e44c5
Igor Lukinykh <onilopic@gmail.com>
2024-02-13 14:17:43 +0900

    Added breadcrumbs

--- src/Controller/Pages/TermsController.php
+++ src/Controller/Pages/TermsController.php
@@ -5,12 +5,26 @@ namespace App\Controller\Pages;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\RouterInterface;
+use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
 
 final class TermsController extends AbstractController
 {
-    #[Route(path:'/terms-of-use', name:'terms-of-use')]
-    public function index(): Response
+    #[Route(path:'/terms-of-use', name:'terms-of-use',
+        options: [
+            'breadcrumb' => [
+                'label' => 'Terms of use',
+                'parent_route' => 'homepage'
+            ]
+        ],
+    )]
+    public function index(Breadcrumbs $breadcrumbs, RouterInterface $router): Response
     {
+        // Simple example
+        $breadcrumbs->addItem("Home", $router->generate("homepage"));
+        // Example without URL
+        $breadcrumbs->addItem("Terms of use");
+
         return $this->render('pages/terms.html.twig', []);
     }
 }
